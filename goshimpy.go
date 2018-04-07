53bdf13fc1a5a698446edadb9ca67328cb29c186
Dion Bramley <dion.bramley@xero.com>
2018-04-07 17:17:04 +1200

    Added json encoding of results to allow more complicated returns from python

--- goshimpy.go
+++ goshimpy.go
@@ -1,6 +1,7 @@
 package goshimpy
 
 import (
+	"encoding/json"
 	"fmt"
 	"os/exec"
 )
@@ -12,14 +13,16 @@ func Call(name, path string, result interface{}, params ...interface{}) error {
 
 	toPrint := params[0]
 
-	cmd := exec.Command("python", "-c", fmt.Sprintf("import %s; print %s.%s(%s)", path, path, name, toPrint))
+	cmd := exec.Command("python", "-c", fmt.Sprintf("import json,%s; print(json.dumps(%s.%s(%s)))", path, path, name, toPrint))
 	fmt.Println(cmd.Args)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
+		fmt.Println(string(out))
 		return err
 	}
 	fmt.Println(string(out))
+	json.Unmarshal(out, result)
 
 	return nil
 
--- goshimpy_test.go
+++ goshimpy_test.go
@@ -1,12 +1,19 @@
 package goshimpy_test
 
 import (
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/dionb/goshimpy"
 )
 
 func TestCall(t *testing.T) {
-	goshimpy.Call("test_print", "test", "\"HELLO WORLD\"")
+	var output string
+	err := goshimpy.Call("test_print", "test", &output, "\"HELLO WORLD\"")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	fmt.Println(output)
 	t.Fail()
 }
